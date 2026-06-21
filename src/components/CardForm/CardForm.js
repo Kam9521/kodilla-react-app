a13d1ce699c547e3a924edf8ee5014d233e121f5
Kamil <szpmytyl@wp.pl>
2026-06-21 13:12:14 +0200

    Add Redux actions for columns and cards

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,20 +1,24 @@
 import styles from "./CardForm.module.scss";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import Button from "../Button/Button";
 import TextInput from "../TextInput/TextInput";
 
 const CardForm = (props) => {
   const [title, setTitle] = useState("");
 
+  const dispatch = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    props.action(
-      {
-        title: title,
+    dispatch({
+      type: "ADD_CARD",
+      payload: {
+        title,
+        columnId: props.columnId,
       },
-      props.columnId,
-    );
+    });
 
     setTitle("");
   };
